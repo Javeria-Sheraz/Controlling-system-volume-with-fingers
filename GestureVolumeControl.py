ad1abed842ccac65e17efdc373c335e8e09066a6
Javeria Sheraz <javsheraz1208@gmail.com>
2025-08-28 20:05:47 +0500

    Update GestureVolumeControl.py

--- GestureVolumeControl.py
+++ GestureVolumeControl.py
@@ -28,24 +28,24 @@ while True:
     hands = output.multi_hand_landmarks
     if hands:
         for hand in hands:
-            drawing_utils.draw_landmarks(image, hand)
+            # drawing_utils.draw_landmarks(image, hand)
             landmarks = hand.landmark
             for id, landmark in enumerate(landmarks):
                 x = int(landmark.x * frameWidth)
                 y = int(landmark.y * frameHeight)
                 # For drawing green circle on forefinger
                 if id ==8:
-                    cv2.circle(img = image, center=(x,y), radius=9, color=(0, 255,0), thickness=3)
+                    cv2.circle(img = image, center=(x,y), radius=8, color=(0, 255,0), thickness=3)
                     x1 = x
                     y1 = y
                 # For drawing red circle on thumb
                 if id == 4:
-                    cv2.circle(img=image, center=(x,y), radius=9, color=(0, 0, 255), thickness=3)
+                    cv2.circle(img=image, center=(x,y), radius=8, color=(0, 0, 255), thickness=3)
                     x2 = x
                     y2 = y
         dist = ((x2-x1)**2 + (y2-y1)**2) ** (0.5) //4
         # for drawing yellow line connecting the two circles
-        cv2.line(image,(x1,y1), (x2,y2), (0,255,255), 4)
+        cv2.line(image,(x1,y1), (x2,y2), (255,0,0), 3)
 
         # Getting current volume level (0.0 to 1.0)
         currentVol = volume.GetMasterVolumeLevelScalar()
@@ -53,14 +53,14 @@ while True:
         # Map distance to volume level
         vol = np.interp(dist, [6, 60], [0.0, 1.0])
         volume.SetMasterVolumeLevelScalar(vol, None)
-        if dist > 30:
+        if dist > 8:
             pyautogui.press("volumeup")
         else:
             pyautogui.press("volumedown")
 
         # This displays text on the video
         cv2.putText(image, f'Vol: {int(vol * 100)}%', (30, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
 
     cv2.imshow("Hand Volume Control", image)
 
